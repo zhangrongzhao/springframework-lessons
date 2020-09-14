93db08b47f4faf6411244a9b04ae5e3200e56314
zhangrongzhao <rongzhao.zhang@allcure.cn>
2020-09-14 18:08:54 +0800

    BeanInfo： BeanDescriptor PropertyDescriptor MethodDescriptor

--- thinking-in-spring/java-beans-demo/src/main/java/org/geekbang/thinking/in/spring/ioc/java/beans/BeanInfoDemo.java
+++ thinking-in-spring/java-beans-demo/src/main/java/org/geekbang/thinking/in/spring/ioc/java/beans/BeanInfoDemo.java
@@ -1,6 +1,8 @@
 package org.geekbang.thinking.in.spring.ioc.java.beans;
 
 import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
 
 /**
  * {@link BeanInfo} 示例
@@ -9,7 +11,8 @@ import java.beans.BeanInfo;
  * @author rongzhao
  * */
 public class BeanInfoDemo {
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws IntrospectionException {
+        BeanInfo beanInfo = Introspector.getBeanInfo(Person.class,Object.class);
+        System.out.println(beanInfo);
     }
 }
