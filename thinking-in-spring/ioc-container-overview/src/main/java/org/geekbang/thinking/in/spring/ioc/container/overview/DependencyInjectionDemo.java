b2b560770fd3eac3c0ae6a36729d9ca2af009a2b
zhangrongzhao <rongzhao.zhang@allcure.cn>
2020-09-15 17:35:54 +0800

    依赖来源： 1. 自定义Bean 2. 容器内建Bean对象 3. 容器内建依赖

--- thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/container/overview/DependencyInjectionDemo.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/container/overview/DependencyInjectionDemo.java
@@ -6,7 +6,12 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 import java.util.Map;
 
@@ -18,20 +23,33 @@ public class DependencyInjectionDemo {
         //配置xml
         //启动应用上下文
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("META-INF/dependency-injection-context.xml");
+        //1.依赖来源：自定义Bean
         UserRepository userRepository = beanFactory.getBean("userRepository",UserRepository.class);
         //System.out.println(userRepository.getUsers());
-        //依赖注入
+        //依赖注入（内建依赖）
         //System.out.println(userRepository.getBeanFactory());
         //System.out.println(userRepository.getBeanFactory()==beanFactory);
-        //依赖查找
+        //依赖查找（错误）
+        //2.依赖来源：内部依赖
         //System.out.println(beanFactory.getBean(BeanFactory.class));
 
         //ObjectFactory<User> userObjectFactory=userRepository.getUserObjectFactory();
         //System.out.println(userObjectFactory.getObject());
 
-        ObjectFactory<ApplicationContext> objectFactory=userRepository.getObjectFactory();
-        System.out.println(objectFactory.getObject());
-        System.out.println(beanFactory);
-        System.out.println(objectFactory.getObject()==beanFactory);
+//        ObjectFactory<ApplicationContext> objectFactory=userRepository.getObjectFactory();
+//        System.out.println(objectFactory.getObject());
+//        System.out.println(beanFactory);
+//        System.out.println(objectFactory.getObject()==beanFactory);
+
+        //容器内建Bean
+        //3.内初初始化的Bean
+        Environment environment = beanFactory.getBean(Environment.class);
+        System.out.println(environment);
+
+        MessageSource messageSource = beanFactory.getBean(MessageSource.class);
+        System.out.println(messageSource);
+
+        ApplicationEventMulticaster multicaster = beanFactory.getBean(ApplicationEventMulticaster.class);
+        System.out.println(multicaster);
     }
 }
